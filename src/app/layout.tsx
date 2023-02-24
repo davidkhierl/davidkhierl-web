81db600abdbd6e0686ec914c01834ab8655a786d
David Khierl <dkrgidor@gmail.com>
2023-02-24 20:01:58 +0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+export const metadata = {
+  title: 'NextJS Tailwind Template',
+  description: 'NextJS Tailwind starter template!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 font-sans dark:bg-slate-900 dark:text-white">
+      <h1 className="mb-4 text-xl font-extrabold  md:text-6xl">
+        <span>🚀</span>
+        <span className="bg-gradient-to-r from-orange-600 to-blue-500 bg-clip-text text-transparent">
+          NextJS Tailwind Template
+        </span>
+      </h1>
+    </main>
+  )
+}
